052003e848961daab1e0d8fbc9388710cc94ef8c
Vitor Valente <dev.vitorlucas@gmail.com>
2025-02-23 16:43:47 -0300

    feat: add function to fetch restaurant by slug

--- src/app/data/get-restaurant-by-slug.tsx
+++ src/app/data/get-restaurant-by-slug.tsx
@@ -1,6 +1,6 @@
 import { db } from "@/lib/prisma";
 
-export async function name(slug: string) {
+export async function GetRestaurantBySlug(slug: string) {
   const restaurant = await db.restaurant.findUnique({ where: { slug: slug } });
   return restaurant;
 }

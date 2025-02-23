d616c4af05795c5b7a92be77c3902d23df0d5cc9
Vitor Valente <dev.vitorlucas@gmail.com>
2025-02-23 19:19:22 -0300

    feat: adds the component that renders the consumption methods

---
+++ src/app/[slug]/_components/consumption-method-option.tsx
@@ -0,0 +1,43 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { ConsumptionMethod } from "@prisma/client";
+import Image from "next/image";
+import Link from "next/link";
+
+interface ConsumptionMethodOptionProps {
+  imageUrl: string;
+  imageAlt: string;
+  buttonText: string;
+  slug: string;
+  option: ConsumptionMethod;
+}
+
+export function ConsumptionMethodOption({
+  imageUrl,
+  imageAlt,
+  buttonText,
+  option,
+  slug,
+}: ConsumptionMethodOptionProps) {
+  return (
+    <Card>
+      <CardContent className="flex items-center justify-center py-8">
+        <div className="relative h-[80px] w-[80px]">
+          <Image
+            className="object-contain"
+            src={imageUrl}
+            fill
+            alt={imageAlt}
+          />
+        </div>
+      </CardContent>
+      <CardFooter className="flex items-center justify-center">
+        <Button variant={"secondary"} asChild>
+          <Link href={`${slug}/menu?consumptionMethod=${option}`} passHref>
+            {buttonText}
+          </Link>
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+}

993e95ba62b9634f46cdac63f55be16ebeac8a5c
Vitor Valente <dev.vitorlucas@gmail.com>
2025-02-25 21:20:16 -0300

    feat: add product screen header component

---
+++ src/app/[slug]/menu/[productId]/_components/product-header.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { ChevronLeft, ScrollText } from "lucide-react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+interface ProductsHeaderProps {
+  src: string;
+  alt: string;
+}
+export function ProdutctHeader({ src, alt }: ProductsHeaderProps) {
+  const router = useRouter();
+
+  const handleGoBack = () => router.back();
+
+  return (
+    <>
+      <Button
+        variant="secondary"
+        size={"icon"}
+        className="absolute left-4 top-4 z-50"
+        onClick={handleGoBack}
+      >
+        <ChevronLeft />
+      </Button>
+      <Image src={src} alt={alt} fill className="object-contain" />
+      <Button
+        variant="secondary"
+        size={"icon"}
+        className="absolute right-4 top-4 z-50"
+      >
+        <ScrollText />
+      </Button>
+    </>
+  );
+}

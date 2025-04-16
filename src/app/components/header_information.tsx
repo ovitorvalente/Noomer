57bf94157d56f53302efa34b2c908417ff92b4ce
ovitorvalente <ovitorvalente@gmail.com>
2025-04-15 22:52:40 -0300

    feat: adds the header_information component to separate the structure presented in the home page.tsx

---
+++ src/app/components/header_information.tsx
@@ -0,0 +1,10 @@
+export function HeaderInformation() {
+  return (
+    <>
+      <div className="fixed left-0 right-0 top-0 bg-red-100 py-2 text-center">
+        <span className="animate-pulse font-bold text-red-500">
+          🚧 Página em construção - Novidades em breve! 🚧
+        </span>
+      </div></>
+  )
+}
